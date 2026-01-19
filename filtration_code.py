108e42ff61d13cfea584a3e607e2ed236eae1a60
Tijn de Vreeze <tijndevreeze@gmail.com>
2026-01-19 17:41:31 +0100

    Created list of dictionairies per politician wikipedia page including name party and number of spouses

--- filtration_code.py
+++ filtration_code.py
@@ -4,9 +4,15 @@ with open("data/A_people.json", encoding='utf-8') as file:
 
 politicians=[]
 
-for dictionairy in data:
-        if "ontology/party_label" in dictionairy:
-                if dictionairy["ontology/party_label"]=="Democratic Party (United States)" or dictionairy["ontology/party_label"]=="Republican Party (United States)":
-                        politicians.append(dictionairy)
+for dictionary in data:
+        if "ontology/party_label" in dictionary:
+                if dictionary["ontology/party_label"]=="Democratic Party (United States)" or dictionary["ontology/party_label"]=="Republican Party (United States)":
+                        politicians.append(dictionary)
 
-print(politicians)
+politician_data=[]
+
+for politician in politicians:
+        if "ontology/spouse_label" in politician:
+                politician_data.append({"name":[politician["title"]], "party":[politician["ontology/party_label"]], "n_spouses":[(len(politician["ontology/spouse_label"]))]})
+#creates a list called "politician_data" that contains dictionairies for each wikipedia page of a politician, their political party, and their number of spouses
+print(politician_data)
