d05df60ac46f8a83fb43b370b07df494e85681da
Emma van der Liet <evanderliet@yahoo.com>
2026-01-19 17:01:29 +0100

    started the code

--- filtration_code.py
+++ filtration_code.py
@@ -1,5 +1,3 @@
-all_people = ["A_people", "B_people", "C_people", "D_people", "E_people", "F_people", "G_people", "H_people", "I_people", "J_people", "K_people", "L_people", "M_people", "N_people", "O_people", "P_people", "Q_people", "R_people", "S_people", "T_people", "U_people", "V_people", "W_people", "X_people", "Y_people", "Z_people"]
 
-for x in all_people:
-    with open(f"{x}.json", encoding='utf-8') as file:
+with open("A_people.json", encoding='utf-8') as file:
         contents = file.read()
