13d7dfd0d41968178d08e4b759f53319a7e733de
Tijn de Vreeze <tijndevreeze@gmail.com>
2026-01-20 17:23:39 +0100

    Created scatter+line plot for spouses and children over the years in pdf

--- visualization_final.r
+++ visualization_final.r
@@ -11,16 +11,30 @@ data <- filter(prefilter, Party == "Democratic Party (United States)"| Party == 
   rename("name" = Name, "party" = Party ,"n_spouses"  = n_Spouses, "n_children" = n_Children, "religion"  = Religion, "birth_year" =  Birthyear) |>
   mutate(n_spouses = as.numeric(n_spouses), n_children = as.numeric(n_children), birth_year = as.numeric(birth_year)) 
 
-data_spouse_over0 <- filter(data, n_spouses>0)
-
-data_spouse_over0_birthyear <- filter(data_spouse_over0, birth_year>0)
-
-data_spouse_over0_birthyear_party <- group_by(data_spouse_over0_birthyear, party, birth_year)|>
-  summarise(avg_spouses_year=mean(n_spouses))
+data_spouse_over0_birthyear_party <- filter(data, n_spouses>0)|>
+  filter(birth_year>0)|>
+  group_by(party, birth_year)|>
+  summarise(avg_spouses_year=mean(n_spouses)) 
 
 ggplot(data_spouse_over0_birthyear_party, aes(x=birth_year, y=avg_spouses_year, color=party)) +
   geom_point()+
+  geom_line()+
   scale_color_manual(values=c("Democratic Party (United States)"="#0008ffff", "Republican Party (United States)"="#ff0000ff"))+
   labs(x="Birth year",
       y="Number of spouses",
     color="Political party")
+ggsave("Number of spouses vs Birthyear per political party.pdf", width=30, height=10, units="cm")
+
+data_children_over0_birthyear_party <- filter(data, n_children>0)|>
+  filter(birth_year>0)|>
+  group_by(party, birth_year)|>
+  summarise(avg_children_year=mean(n_children)) 
+
+ggplot(data_children_over0_birthyear_party, aes(x=birth_year, y=avg_children_year, color=party)) +
+  geom_point()+
+  geom_line()+
+  scale_color_manual(values=c("Democratic Party (United States)"="#0008ffff", "Republican Party (United States)"="#ff0000ff"))+
+  labs(x="Birth year",
+      y="Number of children",
+    color="Political party")
+ggsave("Number of children vs Birthyear per political party.pdf", width=30, height=10, units="cm")
