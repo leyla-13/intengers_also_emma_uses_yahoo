e6f1daf5a89ab6b0166f9ca56e73f3c0b458fd95
Tijn de Vreeze <tijndevreeze@gmail.com>
2026-01-20 14:26:16 +0100

    Fixed dataset to actually be csv format

--- filtration_code.py
+++ filtration_code.py
@@ -1,6 +1,7 @@
 import json
 
 datasets=["A_people", "B_people", "C_people", "D_people", "E_people", "F_people", "G_people", "H_people", "I_people", "J_people", "K_people", "L_people", "M_people", "N_people", "O_people", "P_people", "Q_people", "R_people", "S_people", "T_people", "U_people", "V_people", "W_people", "X_people", "Y_people", "Z_people"]
+politician_data=[]
 for dataset in datasets:
         with open(f"data/{dataset}.json", encoding='utf-8') as file:
                 data = json.load(file)
@@ -9,7 +10,6 @@ for dataset in datasets:
                 if "ontology/party_label" in dictionary:
                         if dictionary["ontology/party_label"]=="Democratic Party (United States)" or dictionary["ontology/party_label"]=="Republican Party (United States)":
                                 politicians.append(dictionary)
-        politician_data=[]
         for politician in politicians:
                 if "ontology/spouse_label" in politician:
                         n_spouses=1
@@ -40,6 +40,7 @@ for dataset in datasets:
 
                 politician_data.append({"name":politician["title"], "party":politician["ontology/party_label"], "n_spouses":n_spouses, "n_children":n_children, "religion":religion, "birthyear":birthyear})
         #creates a list called "politician_data" that contains dictionairies for each wikipedia page of a politician, their political party, and their number of spouses
+with open("politicians_data_all.csv", "w", encoding="utf-8") as file:
+        file.write("Name, Party, n_Spouses, n_Children, Religion, Birthyear \n")
         for dictionary in politician_data:
-                with open("politicians_data_all.csv", "a", encoding="utf-8") as file:
-                        file.write(f"{dictionary} \n")
+                file.write(f"{dictionary['name']}, {dictionary['party']}, {dictionary['n_spouses']}, {dictionary['n_children']}, {dictionary['religion']}, {dictionary['birthyear']} \n")
