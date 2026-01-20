da19799f6bb36f8bc3e98f1a88184b64c39ba71d
“Leyla <durugursan@gmail.com>
2026-01-20 17:18:56 +0100

    new plots!!!!

--- stat_analysis_final.r
+++ stat_analysis_final.r
@@ -28,7 +28,7 @@ dat_spouse <- filter(data, n_spouses > 0) |>
 # of divorces could be good
 # look at distribution of zeros as well
 
-data <- 
+
 
 # find out how to seperate into groups
 # this one is for wives
@@ -47,7 +47,7 @@ data <-
 
 
 
-print(wives_t_test)
+
 
 # this one is for kids
 
@@ -57,17 +57,41 @@ summary_spouse_non <- dat_spouse |>
   group_by(party)|>
   summarise(avg_n_spouses_non = mean(n_spouses, na.rm = TRUE))
 
+summary_kids_non <- dat_spouse |>
+  group_by(party)|>
+  summarise(avg_n_children_non = mean(n_children, na.rm = TRUE))
+
 summary_spouse <- data |>
   group_by(party)|>
   summarise(avg_n_spouses = mean(n_spouses, na.rm = TRUE))
 
-dem_rep_bar <-  data |> 
-  ggplot(mapping = aes(x = party, y = avg_n_spouses)) +
-  geom_col(position = "dodge", color = "#000000ff", fill = "#f68cc8ff") +
-  labs(x = "Grant Status",
-  y = "Graduation Rate") +
-  scale_x_discrete(labels = c ("With Grant", "Without Grant")) +
-  scale_y_continuous(labels = scales::label_percent()) +
-  theme_classic() + #i made it minimal for you guys
+dem_rep_bar <-  summary_spouse_non |> 
+  ggplot(mapping = aes(x = party, y = avg_n_spouses_non)) +
+  geom_col(position = "dodge", color = "#000000ff", fill = "#00000082") +
+  labs(x = "Party",
+  y = "Number of Spouses") +
+  theme_classic() + 
   theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
-ggsave("grad_rate_grant_status_barplot.pdf", plot = grad_rate_bar, width = 8 , height = 10, units = "cm")
+print(dem_rep_bar)
+
+ggsave('bar_plot_spouse.pdf', plot = dem_rep_bar, width = 8 , height = 10, units = "cm")
+
+
+dem_rep_bar_kid <-  summary_kids_non |> 
+  ggplot(mapping = aes(x = party, y = avg_n_children_non)) +
+  geom_col(position = "dodge", color = "#000000ff", fill = "#00000082") +
+  labs(x = "Party",
+  y = "Number of Children") +
+  theme_classic() + 
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
+print(dem_rep_bar_kid)
+
+ggsave('bar_plot_kid.pdf', plot = dem_rep_bar_kid, width = 8 , height = 10, units = "cm")
+
+# year_reg <- 
+#   ggplot(dat_spouse, aes(x=birth_year,y= n_spouses, color=party))+
+#   geom_point(alpha=1)+
+#   geom_smooth(method= "lm")+
+#   scale_color_discrete(labels=c("Democratic Party","Republican Party"))+
+#   theme_minimal() 
+# print(year_reg)
