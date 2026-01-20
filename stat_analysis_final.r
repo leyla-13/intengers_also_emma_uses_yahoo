368c01bd351760fe6643400c12a7072fe9bcedca
“Leyla <durugursan@gmail.com>
2026-01-20 16:35:10 +0100

    summaries

--- stat_analysis_final.r
+++ stat_analysis_final.r
@@ -22,25 +22,28 @@ distinct(data, religion) |>
 
 
 ## spouse and kid data present
-dat_spouse <- filter(data, n_spouses > 0 | n_children > 0) 
+dat_spouse <- filter(data, n_spouses > 0) |>
+  mutate(n_spouses = as.numeric(n_spouses), n_children = as.numeric(n_children), birth_year = as.numeric(birth_year)) 
 
 # of divorces could be good
 # look at distribution of zeros as well
 
+data <- 
+
 # find out how to seperate into groups
 # this one is for wives
 
-spouses_dem <- filter(data, party == "democratic") |>
-  filter(n_spouses > 0)
+# spouses_dem <- filter(data, party == "democratic") |>
+#   filter(n_spouses > 0)
 
-spouses_rep <- filter(data, party == "republican")
+# spouses_rep <- filter(data, party == "republican")
 
 
-# leveneTest(n_spouses ~ party, data = data)
+# # leveneTest(n_spouses ~ party, data = data)
 
-t.test(dems$n_spouses, spouses_rep$n_children, 
-alternative = "greater",
-var.equal=TRUE, data = data)
+# t.test(dems$n_spouses, spouses_rep$n_children, 
+# alternative = "greater",
+# var.equal=TRUE, data = data)
 
 
 
@@ -48,3 +51,23 @@ print(wives_t_test)
 
 # this one is for kids
 
+## bar plot dem-rep difference
+
+summary_spouse_non <- dat_spouse |>
+  group_by(party)|>
+  summarise(avg_n_spouses_non = mean(n_spouses, na.rm = TRUE))
+
+summary_spouse <- data |>
+  group_by(party)|>
+  summarise(avg_n_spouses = mean(n_spouses, na.rm = TRUE))
+
+dem_rep_bar <-  data |> 
+  ggplot(mapping = aes(x = party, y = avg_n_spouses)) +
+  geom_col(position = "dodge", color = "#000000ff", fill = "#f68cc8ff") +
+  labs(x = "Grant Status",
+  y = "Graduation Rate") +
+  scale_x_discrete(labels = c ("With Grant", "Without Grant")) +
+  scale_y_continuous(labels = scales::label_percent()) +
+  theme_classic() + #i made it minimal for you guys
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
+ggsave("grad_rate_grant_status_barplot.pdf", plot = grad_rate_bar, width = 8 , height = 10, units = "cm")
