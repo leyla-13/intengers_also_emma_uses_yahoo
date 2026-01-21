798e72ce3be28a7120d17ac7a8b45538bdfde49d
Nil Hervé <nilherve16@gmail.com>
2026-01-21 17:40:24 +0100

    Formatted barplot

--- visualization_final.r
+++ visualization_final.r
@@ -14,9 +14,9 @@ ggplot(data, aes(x=generation, y=n_spouses, fill=party)) +
   scale_fill_manual(values=c("Democratic Party (United States)"="#1E1BE3", "Republican Party (United States)"="#E31B1B"))+
   labs(x="Generation",
       y="Average number of spouses",
-      fill="Political party")
+      fill="Political party") + scale_x_discrete(labels=c("Baby Boomers", "Generation X", "Generation Y", "Greatest Generation", "Silent Generation"))
   
-# ggsave("Average number of spouses vs Generation per political party.pdf", width=30, height=10, units="cm")
+ggsave("Average number of spouses vs Generation per political party.pdf", width=30, height=10, units="cm")
 
 # ggplot(data_children_over0_birthyear_party, aes(x=birth_year, y=avg_children_year, color=party)) +
 #   geom_point()+
