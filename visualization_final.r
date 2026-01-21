649aacbc45a851a18fd629a6b10a4519d989032a
Tijn de Vreeze <tijndevreeze@gmail.com>
2026-01-21 17:35:08 +0100

    Bar graph per generation

--- visualization_final.r
+++ visualization_final.r
@@ -1,40 +1,28 @@
  library(tidyverse)
 
 
-prefilter <- read_csv('politicians_data_all.csv')
+data <- read_csv('new_cleaner_beautiful_data.csv')
 #filtering data more
 ## data <- filter()
 
 
-#col_names are: 'name', 'party' 'n_spouses', 'n_children', 'religion', 'birthyear'
-data <- filter(prefilter, Party == "Democratic Party (United States)"| Party == "Republican Party (United States)") |>
-  rename("name" = Name, "party" = Party ,"n_spouses"  = n_Spouses, "n_children" = n_Children, "religion"  = Religion, "birth_year" =  Birthyear) |>
-  mutate(n_spouses = as.numeric(n_spouses), n_children = as.numeric(n_children), birth_year = as.numeric(birth_year)) 
+#col_names are: 'Name', 'Party' 'n_Spouses', 'n_Children', 'Religion', 'Birthyear'
 
-data_spouse_over0_birthyear_party <- filter(data, n_spouses>0)|>
-  filter(birth_year>0)|>
-  group_by(party, birth_year)|>
-  summarise(avg_spouses_year=mean(n_spouses)) 
 
-ggplot(data_spouse_over0_birthyear_party, aes(x=birth_year, y=avg_spouses_year, color=party)) +
-  geom_point()+
-  geom_line()+
-  scale_color_manual(values=c("Democratic Party (United States)"="#0008ffff", "Republican Party (United States)"="#ff0000ff"))+
-  labs(x="Birth year",
+ggplot(data, aes(x=generation, y=n_spouses, fill=party)) +
+  geom_bar(stat="summary", fun=mean, position="dodge")+
+  scale_fill_manual(values=c("Democratic Party (United States)"="#1E1BE3", "Republican Party (United States)"="#E31B1B"))+
+  labs(x="Generation",
       y="Average number of spouses",
-    color="Political party")
-ggsave("Average number of spouses vs Birthyear per political party.pdf", width=30, height=10, units="cm")
+      fill="Political party")
+  
+# ggsave("Average number of spouses vs Generation per political party.pdf", width=30, height=10, units="cm")
 
-data_children_over0_birthyear_party <- filter(data, n_children>0)|>
-  filter(birth_year>0)|>
-  group_by(party, birth_year)|>
-  summarise(avg_children_year=mean(n_children)) 
-
-ggplot(data_children_over0_birthyear_party, aes(x=birth_year, y=avg_children_year, color=party)) +
-  geom_point()+
-  geom_line()+
-  scale_color_manual(values=c("Democratic Party (United States)"="#0008ffff", "Republican Party (United States)"="#ff0000ff"))+
-  labs(x="Birth year",
-      y="Average number of children",
-    color="Political party")
-ggsave("Average number of children vs Birthyear per political party.pdf", width=30, height=10, units="cm")
+# ggplot(data_children_over0_birthyear_party, aes(x=birth_year, y=avg_children_year, color=party)) +
+#   geom_point()+
+#   geom_line()+
+#   scale_color_manual(values=c("Democratic Party (United States)"="#1E1BE3", "Republican Party (United States)"="#E31B1B"))+
+#   labs(x="Birth year",
+#       y="Average number of children",
+#     color="Political party")
+# ggsave("Average number of children vs Birthyear per political party.pdf", width=30, height=10, units="cm")
