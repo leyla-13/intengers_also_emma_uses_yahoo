82640b606d8926cffa4be275524f3011d28f23d7
Tijn de Vreeze <tijndevreeze@gmail.com>
2026-01-21 17:59:38 +0100

    Ordered generations chronologically

--- visualization_final.r
+++ visualization_final.r
@@ -1,28 +1,15 @@
  library(tidyverse)
 
 
-data <- read_csv('new_cleaner_beautiful_data.csv')
-#filtering data more
-## data <- filter()
-
-
-#col_names are: 'Name', 'Party' 'n_Spouses', 'n_Children', 'Religion', 'Birthyear'
-
+data <- read_csv('new_cleaner_beautiful_data.csv')|>
+  mutate(generation=fct_relevel(generation, c("greatest_generation", "silent_generation", "baby_boomers", "generation_x", "generation_y")))
 
 ggplot(data, aes(x=generation, y=n_spouses, fill=party)) +
   geom_bar(stat="summary", fun=mean, position="dodge")+
   scale_fill_manual(values=c("Democratic Party (United States)"="#1E1BE3", "Republican Party (United States)"="#E31B1B"))+
   labs(x="Generation",
       y="Average number of spouses",
-      fill="Political party") + scale_x_discrete(labels=c("Baby Boomers", "Generation X", "Generation Y", "Greatest Generation", "Silent Generation"))
+      fill="Political party")+
+  scale_x_discrete(labels=c("Greatest Generation","Silent Generation", "Baby Boomers", "Generation X", "Generation Y"))
   
 ggsave("Average number of spouses vs Generation per political party.pdf", width=30, height=10, units="cm")
-
-# ggplot(data_children_over0_birthyear_party, aes(x=birth_year, y=avg_children_year, color=party)) +
-#   geom_point()+
-#   geom_line()+
-#   scale_color_manual(values=c("Democratic Party (United States)"="#1E1BE3", "Republican Party (United States)"="#E31B1B"))+
-#   labs(x="Birth year",
-#       y="Average number of children",
-#     color="Political party")
-# ggsave("Average number of children vs Birthyear per political party.pdf", width=30, height=10, units="cm")
