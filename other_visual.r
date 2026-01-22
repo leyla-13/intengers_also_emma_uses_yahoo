fc562fc675304b2b0442c81dd8df11a050cf1b88
“Leyla <durugursan@gmail.com>
2026-01-22 11:04:28 +0100

    data with dummy

--- other_visual.r
+++ other_visual.r
@@ -1,14 +1,49 @@
+install.packages("jtools")
 library(tidyverse)
+library(ggplot2)
+library(jtools)
 
-new_dat <- read_csv("new_cleaner_beautiful_data.csv")
+## birth year grouping by generation
+# dummy_make <- function(n_spouses) {
+#   if (is.na(n_spouses)) {
+#     return(NA)
+#   }
+#   if (n_spouses = 1) {
+#     return(0)
+#   } else if (n_spouses > 1) {
+#     return(1)
+#   }else {
+#     return(NA)
+#   }
+# }
+
+
+  
+new_dat <- read_csv("new_cleaner_beautiful_data.csv") |>
+  mutate(dummy_spouse = case_when(
+    n_spouses == 1 ~ 0,
+    n_spouses > 1 ~ 1,
+    TRUE ~ NA
+  )) |> 
+  mutate(dummy_child = case_when(
+    n_children == 0 ~ 0,
+    n_children > 0 ~ 1,
+    TRUE ~ NA
+  ))
+
+
+
+
 #looking at the zero distribution for children
 
-# this is a problem for later
+
+# making the variables factors
 order_dat <- new_dat |>
   filter(!is.na(religion)) |> 
   mutate(religion = fct_relevel(religion, c("Conservative Christian", "Progressive Christian", "Undefined Christian", "Non Christian")) 
 )
-  
+
+# distribution in data
 distribution_child <-  new_dat |> 
   ggplot(mapping = aes(x = n_children, fill = party)) +
   geom_bar(position = "dodge") +
@@ -32,7 +67,14 @@ distribution_religion <-  order_dat |>
   labs(x = "Religion Category") +
   scale_fill_manual(values=c("#1E1BE3","#E31B1B")) +
   scale_x_discrete(labels=c("Christian (Conservative)", "Christian (Progressive)", "Christian (Undefined)", "Non-Christian"))+
-  theme_classic() +
+  theme_apa(  legend.pos = "right",
+  legend.use.title = FALSE,
+  legend.font.size = 12,
+  x.font.size = 12,
+  y.font.size = 12,
+  facet.title.size = 12,
+  remove.y.gridlines = TRUE,
+  remove.x.gridlines = TRUE) +
   theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
 
 
@@ -87,8 +129,8 @@ dem_rep_bar <-  summary_spouse_remarriage |>
   geom_col(position = "dodge", color = "#000000ff", fill = "#00000082") +
   labs(x = "Party",
   y = "Number of Spouses") +
-  theme_classic() + 
-  theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5)) 
+
 print(dem_rep_bar)
 
 ggsave('bar_plot_spouse.pdf', plot = dem_rep_bar, width = 8 , height = 10, units = "cm")
@@ -106,3 +148,4 @@ print(dem_rep_bar_kid)
 ggsave('bar_plot_kid.pdf', plot = dem_rep_bar_kid, width = 8 , height = 10, units = "cm")
 
 
+  
