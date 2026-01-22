2c5b6ef86e58425520c791612b00b33454c85062
“Leyla <durugursan@gmail.com>
2026-01-22 12:04:52 +0100

    spouse distribution submitted

--- other_visual.r
+++ other_visual.r
@@ -3,20 +3,6 @@ library(tidyverse)
 library(ggplot2)
 library(jtools)
 
-## birth year grouping by generation
-# dummy_make <- function(n_spouses) {
-#   if (is.na(n_spouses)) {
-#     return(NA)
-#   }
-#   if (n_spouses = 1) {
-#     return(0)
-#   } else if (n_spouses > 1) {
-#     return(1)
-#   }else {
-#     return(NA)
-#   }
-# }
-
 
   
 new_dat <- read_csv("new_cleaner_beautiful_data.csv") |>
@@ -49,15 +35,30 @@ distribution_child <-  new_dat |>
   geom_bar(position = "dodge") +
   labs(x = "children count") +
   scale_fill_manual(values=c("#1E1BE3","#E31B1B")) +
-  theme_classic()
+  theme_apa(  legend.pos = "right",
+  legend.use.title = FALSE,
+  legend.font.size = 12,
+  x.font.size = 12,
+  y.font.size = 12,
+  facet.title.size = 12,
+  remove.y.gridlines = TRUE,
+  remove.x.gridlines = TRUE) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
 
 #looking at the zero distribution for remarriage
 distribution_spouse <-  new_dat |> 
-  ggplot(mapping = aes(x = n_spouses, fill = party)) +
+  ggplot(mapping = aes(x = remarriage, fill = party)) +
   geom_bar(position = "dodge") +
-  labs(x = "remarriage count") +
-  scale_fill_manual(values=c("#1E1BE3","#E31B1B")) +
-  theme_classic()
+  labs(x = "Number of Remarriages", y = "Number of Remarriages", fill=" Political Party") +
+  scale_fill_manual(values=c("#1E1BE3","#E31B1B"), labels = c("Democratic", "Republican")) +
+  theme_apa(  legend.pos = "right",
+  legend.use.title = TRUE,
+  legend.font.size = 12,
+  x.font.size = 12,
+  y.font.size = 12,
+  facet.title.size = 12,
+  remove.y.gridlines = TRUE,
+  remove.x.gridlines = TRUE) 
 
 
 #religion distribution per party
@@ -107,33 +108,45 @@ print(sum_rep)
 # colors per column
 
 #remarriage comparison between dems and reps
-summary_spouse_remarriage <- new_dat |>
-  group_by(party)|>
-  summarise(avg_n_remarriage = mean(remarriage, na.rm = TRUE))
+# summary_spouse_remarriage <- new_dat |>
+#   group_by(party)|>
+#   summarise(avg_n_remarriage = mean(remarriage, na.rm = TRUE))
 
-#average children per party
-summary_kids_non <- new_dat |>
-  group_by(party)|>
-  summarise(avg_n_children_non = mean(n_children, na.rm = TRUE))
+# #average children per party
+# summary_kids_non <- new_dat |>
+#   group_by(party)|>
+#   summarise(avg_n_children_non = mean(n_children, na.rm = TRUE))
 
 #remarriage comparison between dems and reps
-summary_spouse_remarriage <- new_dat |>
-  group_by(party)|>
-  summarise(avg_n_remarriage = mean(remarriage, na.rm = TRUE))
+relative_remarriage <- new_dat |>
+  group_by(religion, party)|>
+  summarise(relative_remarriage = mean(dummy_spouse, na.rm = TRUE)) |>
+  mutate(percentage = 100 * relative_remarriage)
 
 
 
 #bar graphs for remarriage
-dem_rep_bar <-  summary_spouse_remarriage |> 
-  ggplot(mapping = aes(x = party, y = avg_n_remarriage)) +
-  geom_col(position = "dodge", color = "#000000ff", fill = "#00000082") +
-  labs(x = "Party",
-  y = "Number of Spouses") +
-  theme(axis.text.x = element_text(angle = 45, vjust = 0.5)) 
+dem_rep_bar <-  relative_remarriage |> 
+  ggplot(mapping = aes(x = religion, y = relative_remarriage, fill = party)) +
+  geom_col(position = "dodge") +
+  labs(x = "Religion Category",
+  y = "Percentage of Remarried Politicians") +
+  scale_y_continuous(labels = scales::label_percent()) +
+  scale_fill_manual(values=c("#1E1BE3","#E31B1B")) +
+  theme_apa(  legend.pos = "right",
+  legend.use.title = FALSE,
+  legend.font.size = 12,
+  x.font.size = 12,
+  y.font.size = 12,
+  facet.title.size = 12,
+  remove.y.gridlines = TRUE,
+  remove.x.gridlines = TRUE) +
+  theme(axis.text.x = element_text(angle = 15, vjust = 0.5)) +
+  geom_text(aes(label=round(percentage, 2)), vjust= 1, hjust = 1)
 
 print(dem_rep_bar)
 
-ggsave('bar_plot_spouse.pdf', plot = dem_rep_bar, width = 8 , height = 10, units = "cm")
+ggsave('bar_plot_spouse.pdf', plot = dem_rep_bar, width = 25 , height = 10, units = "cm")
 
 #bar graph for kid number
 dem_rep_bar_kid <-  summary_kids_non |> 
@@ -141,8 +154,16 @@ dem_rep_bar_kid <-  summary_kids_non |>
   geom_col(position = "dodge", color = "#000000ff", fill = "#00000082") +
   labs(x = "Party",
   y = "Number of Children") +
-  theme_classic() + 
+    theme_apa(  legend.pos = "right",
+  legend.use.title = FALSE,
+  legend.font.size = 12,
+  x.font.size = 12,
+  y.font.size = 12,
+  facet.title.size = 12,
+  remove.y.gridlines = TRUE,
+  remove.x.gridlines = TRUE) +
   theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
+
 print(dem_rep_bar_kid)
 
 ggsave('bar_plot_kid.pdf', plot = dem_rep_bar_kid, width = 8 , height = 10, units = "cm")
