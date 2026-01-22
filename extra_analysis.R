8e691c235f58eb5fe4551772bee75833fd2a7ba1
Tijn de Vreeze <tijndevreeze@gmail.com>
2026-01-22 11:36:42 +0100

    Generation graph as percentage not average. Calculated some additional percentages in extra_analysis

--- extra_analysis.R
+++ extra_analysis.R
@@ -1,18 +1,49 @@
 library(tidyverse)
-
+#this uses the less cleaned version, it does include the data for campaigns and presidencies
 data_all <- read_csv("politicians_data_all_rewritten.csv")
-# print(data_all)
 
-data <- filter(data_all,Party == "Republican Party (United States)") |>
+data_great_reversal <- filter(data_all, Birthyear >= 1901)
+
+#percentage no spouses of total republicans
+data <- filter(data_great_reversal,Party == "Republican Party (United States)") |>
+  group_by(Party, n_Spouses)|>
+  summarise(n=n())|>
+  mutate(percentage=(n/sum(n))*100)|>
+  print()
+
+#percentage more than 1 spouse of all republicans with spouses
+data <- filter(data_great_reversal,Party == "Republican Party (United States)") |>
   filter(n_Spouses>0)|>
   group_by(Party, n_Spouses)|>
   summarise(n=n())|>
   mutate(percentage=(n/sum(n))*100)|>
   print()
 
-data <- filter(data_all,Party == "Democratic Party (United States)") |>
+#percentage no spouses of total democrats
+data <- filter(data_great_reversal,Party == "Democratic Party (United States)") |>
+  group_by(Party, n_Spouses)|>
+  summarise(n=n())|>
+  mutate(percentage=(n/sum(n))*100)|>
+  print()
+
+#percentage more than 1 spouse of all democrats with spouses
+data <- filter(data_great_reversal,Party == "Democratic Party (United States)") |>
   filter(n_Spouses>0)|>
   group_by(Party, n_Spouses)|>
+  summarise(n=n())|>
+  mutate(percentage=(n/sum(n))*100)|>
+  print()
+
+#percentage of republicans that have spouse AND child
+data <- filter(data_great_reversal, Party == "Republican Party (United States)", n_Spouses>0)|>
+  group_by(Party, n_Spouses, n_Children)|>
+  summarise(n=n())|>
+  mutate(percentage=(n/sum(n))*100)|>
+  print()
+
+#percentage of democrats that have spouse AND child
+data <- filter(data_great_reversal, Party == "Democratic Party (United States)", n_Spouses>0)|>
+  group_by(Party, n_Spouses, n_Children)|>
   summarise(n=n())|>
   mutate(percentage=(n/sum(n))*100)|>
   print()
--- visualization_final.r
+++ visualization_final.r
@@ -1,15 +1,31 @@
  library(tidyverse)
 
+#rewrite to get proportion of people with more than one spouse
+  
 
-data <- read_csv('new_cleaner_beautiful_data.csv')|>
-  mutate(generation=fct_relevel(generation, c("greatest_generation", "silent_generation", "baby_boomers", "generation_x", "generation_y")))
+data <- read_csv("new_cleaner_beautiful_data.csv") |>
+  mutate(generation=fct_relevel(generation, c("greatest_generation", "silent_generation", "baby_boomers", "generation_x", "generation_y")))|>
+  mutate(dummy_spouse = case_when(
+    n_spouses == 1 ~ 0,
+    n_spouses > 1 ~ 1,
+    TRUE ~ NA
+  )) |> 
+  mutate(dummy_child = case_when(
+    n_children == 0 ~ 0,
+    n_children > 0 ~ 1,
+    TRUE ~ NA
+  ))|>
+  filter(generation=="greatest_generation"|generation=="silent_generation"|generation=="baby_boomers"|generation=="generation_x")
 
-ggplot(data, aes(x=generation, y=n_spouses, fill=party)) +
+
+
+ggplot(data, aes(x=generation, y=dummy_spouse, fill=party)) +
   geom_bar(stat="summary", fun=mean, position="dodge")+
   scale_fill_manual(values=c("Democratic Party (United States)"="#1E1BE3", "Republican Party (United States)"="#E31B1B"))+
   labs(x="Generation",
-      y="Average number of spouses",
+      y="Percentage of politicians \n that remarried",
       fill="Political party")+
-  scale_x_discrete(labels=c("Greatest Generation","Silent Generation", "Baby Boomers", "Generation X", "Generation Y"))
+  scale_x_discrete(labels=c("Greatest Generation","Silent Generation", "Baby Boomers", "Generation X", "Generation Y"))+
+  scale_y_continuous(labels=scales::label_percent())
   
-ggsave("Average number of spouses vs Generation per political party.pdf", width=30, height=10, units="cm")
+ggsave("Percentage of politicians that remarried vs Generation per political party.pdf", width=30, height=10, units="cm")
